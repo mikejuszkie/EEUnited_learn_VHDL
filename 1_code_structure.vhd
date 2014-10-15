841e173fec0ddd3caf0fa9a028d2a88d4dd7151f
mikejuszkie <mikejuszkie@gmail.com>
2014-10-15 19:13:33 -0400

    reformated lesson1 and fixed comments

--- 1_code_structure.vhd
+++ 1_code_structure.vhd
@@ -1,21 +1,21 @@
-—————————————————————————————————————————————————————————————————
-— VHDL code structure example code
-— EEUnited.tumblr.com
-—
-— This file is an example of the structures inside of a VHDL
-— Code Example Based on examples from the first chapter of 
-— “Circuit Design with VHDL” by Volnei A. Pedroni
-—
-—————————————————————————————————————————————————————————————————
+-----------------------------------------------------------------
+-- VHDL code structure example code
+-- EEUnited.tumblr.com
+--
+-- This file is an example of the structures inside of a VHDL
+-- Code Example Based on examples from the first chapter of
+-- "Circuit Design with VHDL” by Volnei A. Pedroni
+--
+-----------------------------------------------------------------
 
-— Library Declarations
-—————————————————————————————————————————————————————————————————
-LIBRARY library_name; —Comment
-USE library_name.package_name.package_parts; —comment
+-- Library Declarations
+-----------------------------------------------------------------
+LIBRARY library_name; -- Comment
+USE library_name.package_name.package_parts; -- Comment
 
 
-— Entity
-—————————————————————————————————————————————————————————————————
+-- Entity
+-----------------------------------------------------------------
 ENTITY entity_name IS
 PORT (
 Port_name : signal_mode signal_type;
@@ -24,8 +24,8 @@ Port_name : signal_mode signal_type;
 END entity_name;
 
 
-— Architecture
-—————————————————————————————————————————————————————————————————
+-- Architecture
+-----------------------------------------------------------------
 ARCHITECTURE architecture_name OF entity_name IS
 [declarations]
 BEGIN
